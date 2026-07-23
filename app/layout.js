8a47e07d5e2cd129f25fdb503a412770dfdfe7ad
lalit-kishore-kumawat <lalitkishorek7@gmail.com>
2026-07-23 12:28:54 +0530

    feat: add Google Analytics 4 (G-6EJH4HQKCX) via next/script

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata = {
@@ -22,7 +23,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6EJH4HQKCX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6EJH4HQKCX');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   )
 }
